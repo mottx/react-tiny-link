17c05bccc6a60a0acc0779fc278ee315c0fe7bad
Matt Hsieh <matt.hsieh@mottx.com>
2021-06-22 15:04:40 +0800

    Add react-dom to external dependency

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 module.exports = merge(require('./webpack.config.base'), {
     externals: {
         "react": "react",
+        "react-dom": "react-dom",
         "prop-types": "prop-types",
     }
 });
